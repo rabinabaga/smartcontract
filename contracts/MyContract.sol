81ab41206f1ecc1ca4e5d20a4d3aec08a085be62
rabinabaga <rabinabaga@gmail.com>
2024-12-14 22:15:27 +0545

    payment with student id given

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,14 +2,33 @@
 pragma solidity ^0.8.0;
 
    contract MyContract {
-    string public message = "Hello, World!";
+    struct Payment {
+    uint256 amount;
+    string studentId;
+    uint256 timestamp;
+}
+    event FeePaymentStarted(address indexed payer, string studentId, uint256 timestamp);
+    event FeePaymentCompleted(address indexed payer, string studentId, uint256 amount, uint256 timestamp);
 
-    function helloWorld() public view returns (string memory) {
-        return message;
-    }
+   
+mapping(string => Payment) public payments;
 
-    function sum(uint firstNum, uint secondNum) public pure returns (uint ){
-        return firstNum+secondNum;
-    }
+function payFees(string memory studentId) public payable {
+    require(msg.value > 0, "Fee amount must be greater than zero");
+       emit FeePaymentStarted(msg.sender, studentId, block.timestamp);
+
+    payments[studentId] = Payment({
+        amount: msg.value,
+        studentId: studentId,
+        timestamp: block.timestamp
+    });
+      emit FeePaymentCompleted(msg.sender, studentId, msg.value, block.timestamp);
+    
+}
+
+function getPaymentStatus(string memory studentId) public view returns (Payment memory) {
+    return payments[studentId];
+}
+
 
 }
